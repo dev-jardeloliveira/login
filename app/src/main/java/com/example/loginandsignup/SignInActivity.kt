34a450d983b2f314d92e1b24c038de52dae0b799
Jardel Oliveira <dev.jardeloliveira@gmail.com>
2023-07-04 17:48:25 -0300

    Biometric

--- app/src/main/java/com/example/loginandsignup/SignInActivity.kt
+++ app/src/main/java/com/example/loginandsignup/SignInActivity.kt
@@ -6,23 +6,33 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
 import android.widget.Toast
+import androidx.biometric.BiometricPrompt
+import androidx.biometric.BiometricPrompt.PromptInfo
+import androidx.core.content.ContextCompat
 import com.example.loginandsignup.databinding.ActivitySignInBinding
 import com.google.firebase.auth.FirebaseAuth
+import java.util.concurrent.Executor
 
 class SignInActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySignInBinding
     private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var biometricPrompt: BiometricPrompt
+    private lateinit var promptInfo: PromptInfo
+    private lateinit var executor: Executor
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignInBinding.inflate(layoutInflater)
         setContentView(binding.root)
         firebaseAuth = FirebaseAuth.getInstance()
+        executor = ContextCompat.getMainExecutor(this)
+
+        Biometric()
         OnClick()
         SignUp()
 
     }
 
-    fun OnClick(){
+   fun OnClick(){
         binding.button.setOnClickListener{
             val email = binding.email.text.toString()
             val pw = binding.password.text.toString()
@@ -45,6 +55,29 @@ class SignInActivity : AppCompatActivity() {
         }
     }
 
+    fun Biometric(){
+        biometricPrompt = BiometricPrompt(this@SignInActivity, executor,object:BiometricPrompt.AuthenticationCallback(){
+            override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
+                super.onAuthenticationError(errorCode, errString)
+
+            }
+
+            override fun onAuthenticationFailed() {
+                super.onAuthenticationFailed()
+            }
+
+            override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
+                super.onAuthenticationSucceeded(result)
+                SignUp()
+            }
+        })
+        promptInfo = androidx.biometric.BiometricPrompt.PromptInfo.Builder()
+            .setTitle("Biometric Authentication")
+            .setSubtitle("Login using fingerprint or face")
+            .setNegativeButtonText("Cancel")
+            .build()
+        biometricPrompt.authenticate(promptInfo)
+    }
     fun SignUp(){
         binding.msgTextView.setOnClickListener{
             val intent = Intent(this, SignUpActivity::class.java)
